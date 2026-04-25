36d72496fe7d321aba0d7fafdd777f813e0a4dcd
Matheus <106702848+yTheu@users.noreply.github.com>
2026-04-24 23:50:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                     bg: '#0b0d10',
+                        card: '#14171a',
+                        purple: '#9d4edd',
+                        purpleHover: '#c77dff',
+                        textGray: '#a1a7b3',
+                    }
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            }
+         }
+    }
+}
